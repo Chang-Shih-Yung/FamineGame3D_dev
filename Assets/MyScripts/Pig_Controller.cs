412569a88d80c5453dc4d9eb9b10a7aff9c3c193
Chang-Shih-Yung <henry1010921@gmail.com>
2024-08-15 09:37:32 +0800

    新增野豬與動畫狀態機腳本

--- Assets/MyScripts/Pig_Controller.cs
+++ Assets/MyScripts/Pig_Controller.cs
@@ -25,10 +25,12 @@ public class Pig_Controller : ObjectBase
     public float minX = -5.63f;
     public float maxZ = 5.92f;
     public float minZ = -6.33f;
+    private Vector3 targetPos;
 
     private EnemyState enemyState;
+    //這裡就是動畫狀態機
     //當狀態修改時，進入新狀態要做的拾取
-    //相當於OnEnter
+    //相當於OnEnter，也就是一進入要做的事
     private EnemyState EnemyState
     {
         get => enemyState;
@@ -38,10 +40,22 @@ public class Pig_Controller : ObjectBase
             switch (enemyState)
             {
                 case EnemyState.Idle:
-
+                    //播放動畫
+                    //關閉導航
+                    //歇息一段時間再去巡邏，等於一直循環
+                    animator.CrossFadeInFixedTime("Idle", 0.25f);
+                    navMeshAgent.enabled = false;
+                    Invoke(nameof(GoMove), Random.Range(3f, 10f));
                     break;
                 case EnemyState.Move:
-
+                    //播放動畫
+                    //開啟導航
+                    //獲取巡邏點
+                    //移動到指定目標位置
+                    animator.CrossFadeInFixedTime("Move", 0.25f);
+                    navMeshAgent.enabled = true;
+                    targetPos = GetTargetPos();
+                    navMeshAgent.SetDestination(targetPos);
                     break;
                 case EnemyState.Pursue:
 
@@ -56,28 +70,59 @@ public class Pig_Controller : ObjectBase
 
                     break;
             }
+
         }
     }
-
     private void Start()
     {
-        Hp = 100;
+        EnemyState = EnemyState.Idle;
     }
-    public override void Hurt(int damage)
+    private void Update()
     {
-        base.Hurt(damage);
-        animator.SetTrigger("Hurt");
-        PlayAudio(0);
+        //更新
+        StateOnUpdate();
     }
-    protected override void Dead()
+    //用於每一幀更新時根據當前狀態執行相應的行為
+    //因此 StateOnUpdate 方法通常會在 Update 方法中被調用
+    private void StateOnUpdate()
     {
-        base.Dead();
-        PlayAudioDead(0);
-        StartCoroutine(DestroyAfterAudio());
+        
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+
+                break;
+            case EnemyState.Move:
+            //判斷到達時（頭、尾），小於1.5f就待機
+            //1.5f是單位距離
+                if (Vector3.Distance(transform.position, targetPos) < 1.5f)
+                {
+                    EnemyState = EnemyState.Idle;
+                }
+                break;
+            case EnemyState.Pursue:
+
+                break;
+            case EnemyState.Attack:
+
+                break;
+            case EnemyState.Hurt:
+
+                break;
+            case EnemyState.Dead:
+
+                break;
+        }
     }
-    private IEnumerator DestroyAfterAudio()
+    private void GoMove()
     {
-        yield return new WaitForSeconds(0.5f);
-        Destroy(gameObject);
+        EnemyState = EnemyState.Move;
     }
+    //獲取一個範圍內的隨機點
+    //獲取vector3直接return值就可以了
+    private Vector3 GetTargetPos()
+    {
+        return new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
+    }
+
 }
