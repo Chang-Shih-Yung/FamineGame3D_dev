129f3d9c6bcc193a4075e5c9c20c45752e09c85f
Chang-Shih-Yung <henry1010921@gmail.com>
2024-08-17 11:48:42 +0800

    mac-test

--- Assets/MyScripts/Pig_Controller.cs
+++ Assets/MyScripts/Pig_Controller.cs
@@ -42,7 +42,7 @@ public class Pig_Controller : ObjectBase
     public float maxX = 4.74f;
     public float minX = -5.63f;
     public float maxZ = 5.92f;
-    public float minZ = -6.38f;
+    public float minZ = -6.36f;
 
     //目標位置
     private Vector3 targetPos;
